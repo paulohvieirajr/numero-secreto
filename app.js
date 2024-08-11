ff56fd59f3599811ddafd5e0b6e7cf6c58ac72ee
Paulo Vieira <paulohvieirajr@hotmail.com>
2024-08-11 16:22:51 -0300

    Alterando código

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
-
+ 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
